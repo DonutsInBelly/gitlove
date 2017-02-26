9cc8661e16be9df749d051d4a153c82c2d079ec3
Carlin Au <carlinau@gmail.com>
2017-02-26 10:52:07 -0500

    updated schema

--- main/user.js
+++ main/user.js
@@ -4,6 +4,10 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   login: String,
+  id: Number,
+  avatar_url: String,
+  repos_url: String,
+  language: String
 });
 
 module.exports = mongoose.model('User', userSchema);
