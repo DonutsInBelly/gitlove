ad1ecbec8cd9d10ca9e2d44afde4796f83b49aff
Carlin Au <carlinau@gmail.com>
2017-05-17 20:30:10 -0400

    pls display now

--- main/routes.js
+++ main/routes.js
@@ -66,7 +66,7 @@ const init = function RouteHandler(app, passport) {
 
   app.get('/match', isLoggedIn, (req, res)=>{
     findFavLanguage(req.user.repos_url, (results)=>{
-      res.render('match.ejs', { user: req.user, matches: results });
+      res.render('match.ejs', { user: req.user, languages: results });
     });
   });
 }
